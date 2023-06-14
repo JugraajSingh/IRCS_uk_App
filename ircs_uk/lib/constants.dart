95168445405df559cab3641e08031562298d4248
yvsharma02 <yvsharma02@gmail.com>
2023-06-14 15:32:40 +0530

    Remade screen outline

---
+++ ircs_uk/lib/constants.dart
@@ -0,0 +1,18 @@
+class Constants {
+ static const double sideBarWidth = 30;
+ static const double topBottomBarHeight = 30;
+
+ static const double topBottomBarMargin = 50;
+ static const double sideBarMargin = 150;
+  
+ static const double dividerHeight = 5;
+
+  static const double topLogoSize = 50;
+
+  static const double topLabelHeight = 50;
+  static const double topLabelWidth = 300;
+
+  static const double labelsStartPos = topBottomBarHeight + 5;
+  static const double contentStartPost = labelsStartPos + topLabelHeight * 2 + dividerHeight + 15;
+
+}
--- ircs_uk/lib/screen_outline.dart
+++ ircs_uk/lib/screen_outline.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:ircs_uk/assets.dart';
 import 'package:ircs_uk/colors.dart';
+import 'package:ircs_uk/constants.dart';
 
 class ScreenOutline extends StatelessWidget {
-  const ScreenOutline({super.key, required this.isScrollable});
+  const ScreenOutline({super.key, required this.showBottomBar});
 
-  final bool isScrollable;
+  final bool showBottomBar;
 
   @override
   Widget build(BuildContext context) {
@@ -14,7 +15,120 @@ class ScreenOutline extends StatelessWidget {
           gradient: LinearGradient(
               begin: Alignment.topCenter,
               end: Alignment.bottomCenter,
-              colors: [AppColors.basePageGradientTop, AppColors.basePageGradientBottom])),
+              colors: [
+            AppColors.basePageGradientTop,
+            AppColors.basePageGradientBottom
+          ])),
+      child: Stack(fit: StackFit.expand, children: [
+        Positioned(
+            top: Constants.contentStartPost,
+            bottom: Constants.topBottomBarHeight,
+            left: Constants.sideBarMargin,
+            right: Constants.sideBarMargin,
+            child: SingleChildScrollView(
+              child: Column(
+                children: [for (int i = 0; i < 50; i++) Text(i.toString())],
+              ),
+            )),
+        Positioned(
+            left: 0,
+            right: 0,
+            top: Constants.labelsStartPos,
+            child: Column(
+              children: [
+                Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Image.asset(
+                  Assets.screen_base_logo,
+                  height: Constants.topLogoSize,
+                  width: Constants.topLogoSize,
+                ),
+                Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Image.asset(
+                      Assets.screen_base_label,
+                      height: Constants.topLabelHeight,
+                      width: Constants.topLabelWidth,
+                    ),
+                    Image.asset(Assets.screen_base_label,
+                        height: Constants.topLabelHeight, width: Constants.topLabelWidth)
+                  ],
+                )
+              ],
+            ),
+          ),
+                Container(
+                  height: Constants.dividerHeight,
+                  color: AppColors.sideBarColor,
+                ),
+              ],
+            )),
+        Positioned(
+          top: 0,
+          left: Constants.topBottomBarMargin,
+          right: Constants.topBottomBarMargin,
+          child: Container(
+              height: Constants.topBottomBarHeight,
+              decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(20),
+                      bottomRight: Radius.circular(20)),
+                  color: AppColors.sideBarColor)),
+        ),
+        Positioned(
+          top: Constants.sideBarMargin,
+          left: 0,
+          bottom: Constants.sideBarMargin,
+          child: Container(
+              width: Constants.sideBarWidth,
+              decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(20),
+                      bottomRight: Radius.circular(20)),
+                  color: AppColors.sideBarColor)),
+        ),
+        Positioned(
+          top: Constants.sideBarMargin,
+          right: 0,
+          bottom: Constants.sideBarMargin,
+          child: Container(
+              width: Constants.sideBarWidth,
+              decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(20),
+                      bottomLeft: Radius.circular(20)),
+                  color: AppColors.sideBarColor)),
+        ),
+        if (showBottomBar)
+          Positioned(
+            bottom: 0,
+            right: Constants.topBottomBarMargin,
+            left: Constants.topBottomBarMargin,
+            child: Container(
+                height: Constants.topBottomBarHeight,
+                decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(20),
+                        topLeft: Radius.circular(20)),
+                    color: AppColors.sideBarColor)),
+          )
+      ]),
+    );
+/*
+    return Container(
+      decoration: const BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+            AppColors.basePageGradientTop,
+            AppColors.basePageGradientBottom
+          ])),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         mainAxisSize: MainAxisSize.max,
@@ -85,18 +199,19 @@ class ScreenOutline extends StatelessWidget {
               )
             ]),
           )),
-          if (isScrollable)
-          Container(
-            height: 20,
-            width: 350,
-            decoration: const BoxDecoration(
-                color: AppColors.sideBarColor,
-                borderRadius: BorderRadius.only(
-                    topRight: Radius.circular(20),
-                    topLeft: Radius.circular(20))),
-          )
+          if (showBottomBar)
+            Container(
+              height: 20,
+              width: 350,
+              decoration: const BoxDecoration(
+                  color: AppColors.sideBarColor,
+                  borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(20),
+                      topLeft: Radius.circular(20))),
+            )
         ],
       ),
     );
+    */
   }
 }
